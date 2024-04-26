b71941247684c555d27ae040689e3a0a9d68db0e
witKen <sokkenkh12@gmail.com>
2024-04-26 21:26:17 +0700

    Added Road

--- main.js
+++ main.js
@@ -2,14 +2,17 @@ const canvas = document.getElementById("myCanvas");
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
-const car = new Car (100, 100, 30, 50);
+const road = new Road(canvas.width/2, canvas.width);
+const car = new Car (road.getLaneCenter(1), 100, 30, 50);
 car.draw(ctx);
 
 animate();
 
 function animate(){
     car.update();
+
     canvas.height = window.innerHeight;
+    road.draw(ctx);
     car.draw(ctx);
     requestAnimationFrame(animate);
 
